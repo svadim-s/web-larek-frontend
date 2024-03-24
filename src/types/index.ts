2e4c3a9c347c40975a617ba15d7ce7450c3c80bd
svadim-s <vadimrazer2001@gmail.com>
2024-03-24 22:41:18 +0300

    documenting the architecture

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export interface IProduct {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
+}
+
+export interface IBasket {
+  items: string[];
+  total: number;
+}
+
+export interface IOrder {
+  payment: 'cash' | 'card';
+  email: string;
+  phone: string;
+  address: string;
+  items: string[];
+  total: number;
+}
+
+export type OrderForm = Omit<IOrder, 'total' | 'items'>;
+
+export interface IOrderResult {
+  id: string;
+  total: number;
+}
